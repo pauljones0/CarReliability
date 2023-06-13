35178b39656df031cdd8e77fd36f9954cd5c2e0e
Paul Jones <bethekind@gmail.com>
2023-06-12 20:20:44 -0700

    Got bisection working. Now working on refactoring the main.py and the LineDetector.py to get them working as expected.

--- testMidpointGeneration.py
+++ testMidpointGeneration.py
@@ -12,7 +12,7 @@ class TestMidpoints:
                    703, 734, 766, 797, 829, 860, 891, 1000]
         ground_truth = [37.5, 90.5, 122, 153.5, 184.5, 216, 247.5, 279, 310.5, 341.5, 373, 404.5, 436, 467.5, 498.5,
                         530, 561.5, 593, 624.5, 656, 687.5, 718.5, 750, 781.5, 813, 844.5, 875.5, 945.5]
-        assert (calculate_midpoints(numbers) == ground_truth)
+        assert(calculate_midpoints(numbers) == ground_truth)
 
 
 TestMidpoints().test_calculate_midpoints()
